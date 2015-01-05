f01b395dabbf7137647ffa54285fe35546e5a236
Vlad Palos <vladpalos@gmail.com>
2015-01-05 08:52:56 +0200

    Fixed code errors

--- kernel.c
+++ kernel.c
@@ -2,8 +2,9 @@
 *  kernel.c
 */
 
-void clearScreen() {
-	for (unsigned int j = 0; j < 80 * 25 * 2; j += 2) {
+void clearScreen(char *vidptr) {
+	unsigned short int j;
+	for (j = 0; j < 80 * 25 * 2; j += 2) {
 		vidptr[j] = ' ';
 		vidptr[j+1] = 0x07; 			
 	}	
@@ -13,9 +14,11 @@ void kmain(void)
 {
 	char *str = "Welcome ...                                                                     FlyOS (V.Palos)";
 	char *vidptr = (char*)0xb8000; 	
-	unsigned int i = 0;
+	unsigned short int i = 0, j;
 
-	for (unsigned int j = 0; str[j] != '\0'; j++) {
+	clearScreen(vidptr);
+
+	for (j = 0; str[j] != '\0'; j++) {
 		vidptr[i++] = str[j];
 		vidptr[i++] = 0x09;		
 	}
